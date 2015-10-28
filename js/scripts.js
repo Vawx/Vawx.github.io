52937256c563acaf45a47472fb789304c1156607
Kyle Langley <kyle@emotionalrobots.com>
2015-10-28 16:07:27 -0700

    This commit includes css, fonts, js, spec, and index files as well as core logic for ping-pong assignment using chai and mocha

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var pingPong = function( value ) {
+  if ( value % 15 === 0 ) { return 'pingpong'; }
+  if ( value % 3 === 0 )  { return 'ping'; }
+  if ( value % 5 === 0 )  { return 'pong'; }
+  return value;
+};
+
+$( document ).ready( function( ) {
+  $( "form#ping-pong" ).submit( function( event ) {
+    var warning = $( "#warning" ).hide( );
+    var valueList = $( "#value-list" ).empty( );
+    var value = parseInt( $( "input#value" ).val( ) );
+
+    if ( isNaN( value ) ) {
+      warning.show( );
+    } else {
+      for ( var i = 1; i < value + 1; i++ ) {
+        var result = pingPong( i );
+        valueList.append( '<li>' + result + '</li>' );
+      }
+    }
+    
+    event.preventDefault( );
+  });
+});
---
+++ spec/specs.js
@@ -0,0 +1,18 @@
+describe( 'pingPong', function( )
+{
+  it( "Is 'pingpong' if divisible by 15.", function( ) {
+    expect( pingPong( 15 ) ).to.equal( 'pingpong' );
+  });
+
+  it( "Is 'ping' if divisible by 3.", function( ) {
+    expect( pingPong( 3 ) ).to.equal( 'ping' );
+  });
+
+  it( "Is 'pong' if divisble by 5.", function( ) {
+    expect( pingPong( 5 ) ).to.equal( 'pong' );
+  });
+
+  it( "Stays value if not divisble by 3, 5, or 15.", function( ) {
+    expect( pingPong( 14 ) ).to.equal( 14 );
+  });
+});
